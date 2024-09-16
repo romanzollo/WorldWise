8b2fdd8a44660ecbaffb8aa7ea32014d146497fc
Roman Zlagodukhin <zloyandmad@yandex.ru>
2024-09-16 21:40:02 +0200

    creating a CitiesContext

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { useEffect, useState } from 'react';
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 
 import Homepage from './pages/Homepage';
@@ -11,65 +10,35 @@ import CityList from './components/CityList';
 import CountryList from './components/CountryList';
 import City from './components/City';
 import Form from './components/Form';
-
-const BASE_URL = 'http://localhost:9000';
+import { CitiesProvider } from './contexts/CitiesContext';
 
 function App() {
-    const [cities, setCities] = useState([]);
-    const [isLoading, setIsLoading] = useState(false);
-
-    useEffect(() => {
-        async function fetchCities() {
-            try {
-                setIsLoading(true);
-
-                const res = await fetch(`${BASE_URL}/cities`);
-                const data = await res.json();
-                setCities(data);
-            } catch {
-                alert('There was an error loading data...');
-            } finally {
-                setIsLoading(false);
-            }
-        }
-
-        fetchCities();
-    }, []);
-
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route index element={<Homepage />} />
-                <Route path="product" element={<Product />} />
-                <Route path="pricing" element={<Pricing />} />
-                <Route path="login" element={<Login />} />-
-                <Route path="app" element={<AppLayout />}>
-                    {/* Используем компонент Navigate (из 'react-router-dom') для перехода при нажатии на кнопку 'start tracking now' сразу к адресу 'app/cities' и подсвечивания кнопки 'cities' а не просто к адресу 'app'.
+        <CitiesProvider>
+            <BrowserRouter>
+                <Routes>
+                    <Route index element={<Homepage />} />
+                    <Route path="product" element={<Product />} />
+                    <Route path="pricing" element={<Pricing />} />
+                    <Route path="login" element={<Login />} />-
+                    <Route path="app" element={<AppLayout />}>
+                        {/* Используем компонент Navigate (из 'react-router-dom') для перехода при нажатии на кнопку 'start tracking now' сразу к адресу 'app/cities' и подсвечивания кнопки 'cities' а не просто к адресу 'app'.
                     Добавляем к компоненту Navigate 'replace' чтобы можно было возвращаться назад на предыдущую страницу
                     */}
-                    <Route index element={<Navigate to="cities" replace />} />
-                    <Route
-                        path="cities"
-                        element={
-                            <CityList cities={cities} isLoading={isLoading} />
-                        }
-                    />
-                    {/* указываем параметр и элемент который будет отображаться, если URL адрес содержит это параметр */}
-                    <Route path="cities/:id" element={<City />} />
-                    <Route
-                        path="countries"
-                        element={
-                            <CountryList
-                                cities={cities}
-                                isLoading={isLoading}
-                            />
-                        }
-                    />
-                    <Route path="form" element={<Form />} />
-                </Route>
-                <Route path="*" element={<PageNotFound />} />
-            </Routes>
-        </BrowserRouter>
+                        <Route
+                            index
+                            element={<Navigate to="cities" replace />}
+                        />
+                        <Route path="cities" element={<CityList />} />
+                        {/* указываем параметр и элемент который будет отображаться, если URL адрес содержит это параметр */}
+                        <Route path="cities/:id" element={<City />} />
+                        <Route path="countries" element={<CountryList />} />
+                        <Route path="form" element={<Form />} />
+                    </Route>
+                    <Route path="*" element={<PageNotFound />} />
+                </Routes>
+            </BrowserRouter>
+        </CitiesProvider>
     );
 }
 
